a7f2487b61fb26bc7cabbc6f51c98cb76f9038f0
Alicja Bieryt <alicjabieryt@gmail.com>
2024-08-09 21:53:28 +0200

    not found view and vercel.json

--- client/src/views/NotFoundView.vue
+++ client/src/views/NotFoundView.vue
@@ -1,5 +1,8 @@
 <template>
-    <div id="notFoundView"></div>
+    <div id="notFoundView">
+        <h1>404 :(</h1>
+        <h3>Nie znaleziono strony</h3>
+    </div>
 </template>
 
 <style src="./NotFoundView.scss" ></style>
