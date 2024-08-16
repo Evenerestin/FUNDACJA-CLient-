df666f59988fcbd7b5a2620471d5976a4c22a3c5
Alicja Bieryt <alicjabieryt@gmail.com>
2024-08-16 04:24:07 +0200

    mobile navbar draft

--- client/src/Layout/NavbarView.vue
+++ client/src/Layout/NavbarView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, watch } from 'vue'
+import { onBeforeUnmount, onMounted, ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
 
 const accentColor = ref('')
@@ -36,16 +36,38 @@ watch(
   () => route.path,
   (newPath) => {
     setAccentColor(newPath)
+    openMenu.value = false
   }
 )
 setAccentColor(route.path)
 const getAccentColorClass = () => {
   return accentColor.value
 }
+
+const openMenu = ref(false)
+const toggleMenu = () => {
+  openMenu.value = !openMenu.value
+}
+
+const windowWidth = ref(window.innerWidth)
+const isMobile = ref(window.innerWidth <= 1023)
+
+const handleResize = () => {
+  windowWidth.value = window.innerWidth
+  isMobile.value = window.innerWidth <= 1023
+}
+
+onMounted(() => {
+  window.addEventListener('resize', handleResize)
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', handleResize)
+})
 </script>
 
 <template>
-  <nav class="flex">
+  <nav class="flex" :class="{ active: openMenu, inactive: !openMenu }">
     <router-link to="/">
       <button class="siteLogo flexColumn">Anetka Bieryt<span>Ty też możesz pomóc!</span></button>
     </router-link>
@@ -65,9 +87,23 @@ const getAccentColorClass = () => {
           </router-link>
         </li>
       </ul>
-      <a href="https://dzieciom.pl" target="_blank" class="foundationLogo">
-        <img src="/zdazyczpomocalogo.png" alt="Fundacja Dzieciom Logo" />
-      </a>
+    </div>
+    <a href="https://dzieciom.pl" target="_blank" class="foundationLogo">
+      <img src="/zdazyczpomocalogo.png" alt="Fundacja Dzieciom Logo" />
+    </a>
+    <div
+      v-if="isMobile"
+      @click="toggleMenu"
+      :class="{ active: openMenu, inactive: !openMenu }"
+      class="toggleMobile gridCenter"
+    >
+      <svg id="hamburger" viewbox="0 0 60 40">
+        <g stroke-width="4" stroke-linecap="round" stroke-linejoin="round">
+          <path id="top-line" d="M10,15 L40,15 Z"></path>
+          <path id="middle-line" d="M10,25 L40,25 Z"></path>
+          <path id="bottom-line" d="M10,35 L40,35 Z"></path>
+        </g>
+      </svg>
     </div>
   </nav>
 </template>
